34314e6b4e8a1f23c6c1f67a279765b661d8645f
hurricup <hurricup@gmail.com>
2018-04-30 08:43:02 +0300

    2018.1.2: Grammar-Kit 2017.1.3 is now default

--- src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
+++ src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
@@ -1,7 +1,7 @@
 package org.jetbrains.grammarkit
 
 class GrammarKitPluginExtension {
-    def grammarKitRelease = '2017.1.2'
+    def grammarKitRelease = '2017.1.3'
     def jflexRelease = '1.7.0-1'
 }
 
