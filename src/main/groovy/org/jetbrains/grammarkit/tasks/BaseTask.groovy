93584ebd5adefe2cce2b92352cae134a2bc54dc2
Alexandr Evstigneev <hurricup@gmail.com>
2018-09-12 21:47:47 +0300

    2018.2 Generation tasks considers jvmArgs as internal props
    Recent IDEA updates pushes debugging port to the gradle tasks. Because of this every debugging considers generation tasks not upToDate
    
    This solution looks hacky to me, but no better ideas for now

--- src/main/groovy/org/jetbrains/grammarkit/tasks/BaseTask.groovy
+++ src/main/groovy/org/jetbrains/grammarkit/tasks/BaseTask.groovy
@@ -1,5 +1,6 @@
 package org.jetbrains.grammarkit.tasks
 
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.JavaExec
 
 /**
@@ -7,6 +8,12 @@ import org.gradle.api.tasks.JavaExec
  */
 abstract class BaseTask extends JavaExec {
     def purgeOldFiles = false
+
+    @Internal
+    @Override
+    List<String> getJvmArgs() {
+        return super.getJvmArgs()
+    }
 
     protected void purgeFiles(File... files) {
         if (purgeOldFiles) {
