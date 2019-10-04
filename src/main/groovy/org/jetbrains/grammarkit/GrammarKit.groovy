29aff732357be9249ec8606e17043dc1f62948a2
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2019-10-04 13:44:19 +0300

    Release 2019.2.3
    Add jitpack.io repository as very last one
    
    Sometimes it may return http error 522 instead of something useful

--- src/main/groovy/org/jetbrains/grammarkit/GrammarKit.groovy
+++ src/main/groovy/org/jetbrains/grammarkit/GrammarKit.groovy
@@ -7,13 +7,12 @@ import org.gradle.api.plugins.JavaPlugin
 class GrammarKit implements Plugin<Project> {
     @Override
     void apply(Project target) {
-        target.repositories {
-            maven { url 'https://jitpack.io' }
-            maven { url "https://jetbrains.bintray.com/intellij-third-party-dependencies" }
-        }
         def grammarKitExtension = target.extensions.create("grammarKit", GrammarKitPluginExtension.class)
-
         target.afterEvaluate {
+            target.repositories {
+                maven { url "https://jetbrains.bintray.com/intellij-third-party-dependencies" }
+                maven { url 'https://jitpack.io' }
+            }
             target.dependencies.add(
                     JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME,
                     "com.github.JetBrains:Grammar-Kit:${grammarKitExtension.grammarKitRelease}",
--- src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
+++ src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
@@ -1,7 +1,7 @@
 package org.jetbrains.grammarkit
 
 class GrammarKitPluginExtension {
-    def grammarKitRelease = '2019.1.1'
+    def grammarKitRelease = '1206783ca03dc80ce7806504e056f37378c331d3'
     def jflexRelease = '1.7.0-1'
 }
 
