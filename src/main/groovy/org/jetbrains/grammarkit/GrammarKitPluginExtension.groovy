9b1e9717995ff57444dab3d14fab7a150cc857fe
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2020-11-11 11:26:23 +0300

    Release 2020.3
    Default grammar-kit is changed to 2020.3
    
    https://github.com/JetBrains/Grammar-Kit/releases/tag/2020.3

--- src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
+++ src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
@@ -1,7 +1,7 @@
 package org.jetbrains.grammarkit
 
 class GrammarKitPluginExtension {
-    def grammarKitRelease = '2020.1'
+    def grammarKitRelease = '2020.3'
     def jflexRelease = '1.7.0-1'
 }
 
