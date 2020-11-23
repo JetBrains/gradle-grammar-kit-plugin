3f5c4c5b982f112a4b40ee88402a33f3552812eb
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2020-11-23 14:03:17 +0300

    Release 2020.3.1
    Grammar-Kit version advanced to 76494a417287856e5bf016d5de22ad2ae74470f4
    
    Fixes problem building on JitPack with java 11

--- src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
+++ src/main/groovy/org/jetbrains/grammarkit/GrammarKitPluginExtension.groovy
@@ -1,7 +1,7 @@
 package org.jetbrains.grammarkit
 
 class GrammarKitPluginExtension {
-    def grammarKitRelease = '2020.3'
+    def grammarKitRelease = '76494a417287856e5bf016d5de22ad2ae74470f4'
     def jflexRelease = '1.7.0-1'
 }
 
