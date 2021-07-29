34652461f589a6f79dadb97ca7bc2067e3d46524
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-07-29 08:00:46 +0200

    Migrate Gradle configuration to Kotlin DSL, introduce `gradle.properties`

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+    id("java-gradle-plugin")
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "0.15.0"
+    id("org.jetbrains.changelog") version "1.2.1"
+}
+
+//buildscript {
+//    repositories {
+//        maven {
+//            url "https://plugins.gradle.org/m2/"
+//        }
+//    }
+//    dependencies {
+//        classpath "com.gradle.publish:plugin-publish-plugin:0.11.0"
+//    }
+//}
+
+//apply plugin: "com.gradle.plugin-publish"
+//apply plugin: 'groovy'
+
+dependencies {
+    implementation(gradleApi())
+    implementation(localGroovy())
+}
+
+version = properties("version")
+group = properties("group")
+description = "This plugin allows you to generate lexers using JetBrains patched JFLex and parsers using Grammar-Kit."
+
+gradlePlugin {
+    plugins.create("grammarKitPlugin") {
+        id = "org.jetbrains.grammarkit"
+        displayName = "Gradle Grammar-Kit Plugin"
+        implementationClass = "org.jetbrains.grammarkit.GrammarKit"
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/JetBrains/gradle-grammar-kit-plugin"
+    vcsUrl = "https://github.com/JetBrains/gradle-grammar-kit-plugin"
+    description = "Plugin for generating lexers and parsers for IntelliJ plugins"
+    tags = listOf("intellij", "jetbrains", "idea", "Grammar-Kit", "JFlex")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion")
+        distributionUrl = "https://cache-redirector.jetbrains.com/services.gradle.org/distributions/gradle-$gradleVersion-all.zip"
+    }
+}
