b4705a1f8bbee75fb9d599135efcd8a726a1d3e6
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-07-30 18:12:42 +0200

    Kotlin - GenerateParserTask

--- src/main/kotlin/org/jetbrains/grammarkit/tasks/GenerateParserTask.kt
+++ src/main/kotlin/org/jetbrains/grammarkit/tasks/GenerateParserTask.kt
@@ -1,52 +1,82 @@
 package org.jetbrains.grammarkit.tasks
 
 import org.gradle.api.GradleException
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.internal.ConventionTask
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.*
+import java.io.ByteArrayOutputStream
+import javax.inject.Inject
 
-class GenerateParserTask extends ConventionTask {
+open class GenerateParserTask @Inject constructor(objectFactory: ObjectFactory) : ConventionTask() {
 
     @InputFile
-    def source
+    val source: RegularFileProperty = objectFactory.fileProperty()
 
     @OutputDirectory
-    def targetRoot
+    val targetRoot: DirectoryProperty = objectFactory.directoryProperty()
 
     @Input
-    def pathToParser
+    val pathToParser: Property<String> = objectFactory.property(Boolean::class.java)
 
     @Input
-    def pathToPsiRoot
+    val pathToPsiRoot: Property<String> = objectFactory.property(Boolean::class.java)
 
     @OutputFile
     @Optional
-    def parserFile = project.file("$targetRoot/$pathToParser")
+    val parserFile: RegularFileProperty = objectFactory.fileProperty()
+//    def parserFile = project.file("$targetRoot/$pathToParser")
 
     @OutputDirectory
     @Optional
-    def psiDir = project.file("$targetRoot/$pathToPsiRoot")
+    val psiDir: DirectoryProperty = objectFactory.directoryProperty()
+//    def psiDir = project.file("$targetRoot/$pathToPsiRoot")
 
     @Input
     @Optional
-    def purgeOldFiles = false
+    val purgeOldFiles: Property<String> = objectFactory.property(Boolean::class.java)
 
     @TaskAction
-    void generateParser() {
-        def requiredLibs = [
-                "jdom", "trove4j", "junit", "guava", "asm-all", "automaton", "platform-api", "platform-impl",
-                "util", "annotations", "picocontainer", "extensions", "idea", "openapi", "Grammar-Kit",
-                "platform-util-ui", "platform-concurrency", "intellij-deps-fastutil",
-                // CLion unlike IDEA contains `MockProjectEx` in `testFramework.jar` instead of `idea.jar`
-                // so this jar should be in `requiredLibs` list to avoid `NoClassDefFoundError` exception
-                // while parser generation with CLion distribution
-                "testFramework", "3rd-party"
-        ]
+    fun generateParser() {
+        val requiredLibs = listOf(
+            "jdom", "trove4j", "junit", "guava", "asm-all", "automaton", "platform-api", "platform-impl",
+            "util", "annotations", "picocontainer", "extensions", "idea", "openapi", "Grammar-Kit",
+            "platform-util-ui", "platform-concurrency", "intellij-deps-fastutil",
+            // CLion unlike IDEA contains `MockProjectEx` in `testFramework.jar` instead of `idea.jar`
+            // so this jar should be in `requiredLibs` list to avoid `NoClassDefFoundError` exception
+            // while parser generation with CLion distribution
+            "testFramework", "3rd-party"
+        )
 
-        def clspth
+        ByteArrayOutputStream().use { os ->
+            try {
+                project.javaexec {
+                    it.mainClass.set("org.intellij.grammar.Main")
+                    it.args = getArgs()
+                    it.classpath = getClasspath()
+                    it.standardOutput = os
+                }
+            } catch (e: Exception) {
+                throw GradleException(os.toString().trim(), e)
+            }
+
+            println(os.toString())
+
+        }
+    }
+
+    private fun getArgs(): List<String> {
+        return listOf(project.file(targetRoot), source)
+    }
+
+    private fun getClasspath(): FileCollection {
         if (project.configurations.hasProperty("grammarKitClassPath")) {
-            clspth = project.configurations.grammarKitClassPath
+            return project.configurations.grammarKitClassPath
         } else {
-            clspth = project.configurations.compileClasspath.files.findAll({
+            return project.configurations.compileClasspath.files.findAll({
                 for (lib in requiredLibs) {
                     if (it.name.equalsIgnoreCase("${lib}.jar") || it.name.startsWith("${lib}-")) {
                         return true;
@@ -55,24 +85,6 @@ class GenerateParserTask extends ConventionTask {
                 return false;
             })
         }
-
-        def os = new ByteArrayOutputStream()
-
-        println "---------"
-        println clspth
-        println "---------"
-        try {
-            project.javaexec {
-                mainClass = "org.intellij.grammar.Main"
-                args = [project.file(targetRoot), source]
-                classpath = project.files(clspth)
-                standardOutput = os
-            }
-        } catch (Exception e) {
-            throw new GradleException(os.toString().trim(), e)
-        }
-
-        println os.toString()
     }
 }
 
