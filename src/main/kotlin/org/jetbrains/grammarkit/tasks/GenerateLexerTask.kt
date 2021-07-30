7df0148aa9e64541c40102bcbd3da441686ab267
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-07-30 17:40:17 +0200

    Kotlin - GenerateLexerTask

--- src/main/kotlin/org/jetbrains/grammarkit/tasks/GenerateLexerTask.kt
+++ src/main/kotlin/org/jetbrains/grammarkit/tasks/GenerateLexerTask.kt
@@ -1,71 +1,82 @@
 package org.jetbrains.grammarkit.tasks
 
 import org.gradle.api.GradleException
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.internal.ConventionTask
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import java.io.ByteArrayOutputStream
+import javax.inject.Inject
 
-class GenerateLexerTask extends ConventionTask {
+open class GenerateLexerTask @Inject constructor(objectFactory: ObjectFactory) : ConventionTask() {
 
     @OutputDirectory
-    def targetDir
+    val targetDir: DirectoryProperty = objectFactory.directoryProperty()
 
     @OutputFile
-    def targetFile
+    val targetFile: RegularFileProperty = objectFactory.fileProperty()
 
     @Input
-    def targetClass
+    val targetClass: Property<String> = objectFactory.property(String::class.java)
 
     @InputFile
-    def source
+    val source: RegularFileProperty = objectFactory.fileProperty()
 
     @InputFile
     @Optional
-    def skeleton
+    val skeleton: RegularFileProperty = objectFactory.fileProperty()
 
     @Input
     @Optional
-    def purgeOldFiles = false
+    val purgeOldFiles: Property<String> = objectFactory.property(Boolean::class.java)
 
     @TaskAction
-    void generateLexer() {
-        def newArgs = []
-        if (skeleton != null) {
-            def skeletonFile = project.file(skeleton)
-            newArgs.add("--skel")
-            newArgs.add(skeletonFile)
-        }
-        newArgs.add("-d")
-        newArgs.add(project.file(targetDir))
+    fun generateLexer() {
 
-        def flexFile = project.file(source)
-        newArgs.add(flexFile)
-
-        def clspth
-        if (project.configurations.hasProperty("grammarKitClassPath")) {
-            clspth = project.configurations.grammarKitClassPath
-        } else {
-            clspth = project.configurations.compileClasspath.files.findAll(
-                    { it.name.startsWith("jflex") })
-        }
-
-        def os = new ByteArrayOutputStream()
-
-        try {
-            project.javaexec {
-                mainClass = "jflex.Main";
-                args = newArgs
-                classpath = project.files(clspth)
-                standardOutput = os
+        ByteArrayOutputStream().use { os ->
+            try {
+                project.javaexec {
+                    it.mainClass.set("jflex.Main")
+                    it.args = getArgs()
+                    it.classpath = getClasspath()
+                    it.standardOutput = os
+                }
+            } catch (e: Exception) {
+                throw GradleException (os.toString().trim(), e)
             }
-        } catch (Exception e) {
-            throw new GradleException(os.toString().trim(), e)
+
+            println(os.toString())
+        }
+    }
+
+    private fun getArgs(): List<String> {
+        val args = mutableListOf(
+            "-d", targetDir.get(),
+        )
+
+        skeleton.orNull?.let {
+            args.add("--skel")
+            args.add(it)
         }
 
-        println os.toString()
+        args.add(source.get().asFile.cannonicalPath)
+
+        return args
+    }
+
+    private fun getClasspath(): FileCollection {
+        if (project.configurations.hasProperty("grammarKitClassPath")) {
+            return project.configurations.grammarKitClassPath
+        } else {
+            return project.configurations.compileClasspath.files.findAll({ it.name.startsWith("jflex") })
+        }
     }
 }
