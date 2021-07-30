5fbab99ee82c1709b76670f18ad4a5e8ac5adf81
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-07-30 16:51:05 +0200

    Kotlin - GrammarKitPluginExtension, plus lazy properties

--- src/main/kotlin/org/jetbrains/grammarkit/GrammarKitConstants.kt
+++ src/main/kotlin/org/jetbrains/grammarkit/GrammarKitConstants.kt
@@ -4,4 +4,5 @@ object GrammarKitConstants {
     const val GROUP_NAME = "grammarKit"
     const val GENERATE_LEXER_TASK_NAME = "generateLexer"
     const val GENERATE_PARSER_TASK_NAME = "generateParser"
+    const val VERSION_LATEST = "latest"
 }
