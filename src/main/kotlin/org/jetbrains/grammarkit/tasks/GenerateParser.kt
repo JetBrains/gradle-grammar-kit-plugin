429ca855c39ba0ca9dcc276f2f216c206cad085d
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-08-10 09:31:57 +0200

    integration test fixes

--- src/main/kotlin/org/jetbrains/grammarkit/tasks/GenerateParser.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.grammarkit.tasks
-
-import org.gradle.api.model.ObjectFactory
-
-/**
- * @deprecated Use {@link GenerateParserTask}
- */
-@Deprecated("Use GenerateParserTask")
-class GenerateParser(objectFactory: ObjectFactory) : GenerateParserTask(objectFactory)
