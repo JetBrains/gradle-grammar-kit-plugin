162aa0e3b066184a933df0c3f1fa29882036e527
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-08-10 12:46:13 +0200

    configuration cache

--- src/test/kotlin/org/jetbrains/grammarkit/tasks/GenerateLexerTaskSpec.kt
+++ src/test/kotlin/org/jetbrains/grammarkit/tasks/GenerateLexerTaskSpec.kt
@@ -23,20 +23,19 @@ class GenerateLexerTaskSpec : GrammarKitPluginBase() {
         assertTrue(result.output.contains("Writing code to \"${dir.canonicalPath}/gen/org/jetbrains/grammarkit/lexer/GeneratedLexer.java\""))
     }
 
-//    @Test
-//    fun `do not fail on warning by default`() {
-//        buildFile.groovy("""
-//            generateLexer {
-//                source = "${getResourceFile("Example.flex")}"
-//                targetDir = "gen/org/jetbrains/grammarkit/lexer/"
-//                targetClass = "ExampleLexer"
-//                skeleton = "${getResourceFile("idea-flex.skeleton")}"
-//                purgeOldFiles = true
-//            }
-//        """)
-//
-//        val result = build(GrammarKitConstants.GENERATE_LEXER_TASK_NAME)
-//
-//        assertTrue(result.output.contains("> Task :${GrammarKitConstants.GENERATE_LEXER_TASK_NAME}"))
-//    }
+    @Test
+    fun `reuse configuration cache`() {
+        buildFile.groovy("""
+            generateLexer {
+                source = "${getResourceFile("generateLexer/Example.flex")}"
+                targetDir = "gen/org/jetbrains/grammarkit/lexer/"
+                targetClass = "ExampleLexer"
+            }
+        """)
+
+        build(GrammarKitConstants.GENERATE_LEXER_TASK_NAME, "--configuration-cache")
+        val result = build(GrammarKitConstants.GENERATE_LEXER_TASK_NAME, "--configuration-cache")
+
+        assertTrue(result.output.contains("Reusing configuration cache."))
+    }
 }
--- src/test/kotlin/org/jetbrains/grammarkit/tasks/GenerateParserTaskSpec.kt
+++ src/test/kotlin/org/jetbrains/grammarkit/tasks/GenerateParserTaskSpec.kt
@@ -24,20 +24,20 @@ class GenerateParserTaskSpec : GrammarKitPluginBase() {
         assertTrue(result.output.contains("Example.bnf parser generated to ${dir.canonicalPath}/gen"))
     }
 
-//    @Test
-//    fun `do not fail on warning by default`() {
-//        buildFile.groovy("""
-//            generateLexer {
-//                source = "${getResourceFile("Example.flex")}"
-//                targetDir = "gen/org/jetbrains/grammarkit/lexer/"
-//                targetClass = "ExampleLexer"
-//                skeleton = "${getResourceFile("idea-flex.skeleton")}"
-//                purgeOldFiles = true
-//            }
-//        """)
-//
-//        val result = build(GrammarKitConstants.GENERATE_LEXER_TASK_NAME)
-//
-//        assertTrue(result.output.contains("> Task :${GrammarKitConstants.GENERATE_LEXER_TASK_NAME}"))
-//    }
+    @Test
+    fun `reuse configuration cache`() {
+        buildFile.groovy("""
+            generateParser {
+                source = "${getResourceFile("generateParser/Example.bnf")}"
+                targetRoot = "gen"
+                pathToParser = "/org/jetbrains/grammarkit/IgnoreParser.java"
+                pathToPsiRoot = "/org/jetbrains/grammarkit/psi"
+            }
+        """)
+
+        build(GrammarKitConstants.GENERATE_PARSER_TASK_NAME, "--configuration-cache")
+        val result = build(GrammarKitConstants.GENERATE_PARSER_TASK_NAME, "--configuration-cache")
+
+        assertTrue(result.output.contains("Reusing configuration cache."))
+    }
 }
