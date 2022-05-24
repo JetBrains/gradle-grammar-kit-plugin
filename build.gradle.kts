42e04a275e77ebf6b3c272c5c12e661f15b91ccb
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-05-24 07:53:39 +0200

    Gradle: Configure publishing sources and docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key)?.toString()
@@ -6,8 +7,9 @@ plugins {
     kotlin("jvm") version "1.6.21"
     `kotlin-dsl`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "0.21.0"
+    id("com.gradle.plugin-publish") version "1.0.0-rc-2"
     id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.dokka") version "1.6.21"
 }
 
 repositories {
@@ -45,6 +47,23 @@ pluginBundle {
     vcsUrl = "https://github.com/JetBrains/gradle-grammar-kit-plugin"
     description = "Plugin for generating lexers and parsers for IntelliJ plugins"
     tags = listOf("intellij", "jetbrains", "idea", "Grammar-Kit", "JFlex")
+}
+
+val dokkaHtml by tasks.getting(DokkaTask::class)
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+val sourcesJar = tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+artifacts {
+    archives(javadocJar)
+    archives(sourcesJar)
 }
 
 tasks {
