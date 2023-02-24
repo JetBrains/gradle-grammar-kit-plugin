124f3a851a3031ef71b8a1ac7d48c1695ae645f1
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-02-24 16:51:55 +0100

    Bump default JFlex version to `1.9.0` #124

--- src/main/kotlin/org/jetbrains/grammarkit/GrammarKitConstants.kt
+++ src/main/kotlin/org/jetbrains/grammarkit/GrammarKitConstants.kt
@@ -7,7 +7,7 @@ object GrammarKitConstants {
     const val GENERATE_LEXER_TASK_NAME = "generateLexer"
     const val GENERATE_PARSER_TASK_NAME = "generateParser"
     const val GRAMMAR_KIT_DEFAULT_VERSION = "2022.3.1"
-    const val JFLEX_DEFAULT_VERSION = "1.7.0-1"
+    const val JFLEX_DEFAULT_VERSION = "1.9.0"
     const val MINIMAL_SUPPORTED_GRADLE_VERSION = "7.4"
     const val GRAMMAR_KIT_CLASS_PATH_CONFIGURATION_NAME = "grammarKitClassPath"
 }
